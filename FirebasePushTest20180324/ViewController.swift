59274caa7ee39ffe63db35966620d90aa18067cc
JenHao <jenhao.hsiao@gmail.com>
2018-03-26 09:06:59 -0400

    Setup with firebase successfully

---
+++ FirebasePushTest20180324/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebasePushTest20180324
+//
+//  Created by JenHao on 2018-03-24.
+//  Copyright © 2018 JenHao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++
@@ -1,8 +0,0 @@
-# iOS_SNS-Push-Notifications
-
-## Project goals
-
-> - Create a p12 certificate
-> - Insert eh SNS ARN into app delegate
-> - Download the AWS SDK for iOS
-> - Embed the frameworks, import AWSSNS into app
